58ed64f7312ee193cdf38ff1dfaaa90b4e9289e1
Mihaly Dombi <dombimihaly89@gmail.com>
2020-01-13 13:46:45 +0100

    User model is done

---
+++ src/main/java/hu/flowacademy/holidaymanager/models/Holiday.java
@@ -0,0 +1,4 @@
+package hu.flowacademy.holidaymanager.models;
+
+public class Holiday {
+}
---
+++ src/main/java/hu/flowacademy/holidaymanager/models/User.java
@@ -0,0 +1,31 @@
+package hu.flowacademy.holidaymanager.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private String role;
+
+}
