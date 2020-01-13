c53c113369d5ef3905d20625b6f8a25c75e05f5f
Mihaly Dombi <dombimihaly89@gmail.com>
2020-01-13 13:35:25 +0100

    first commit

---
+++ src/main/java/hu/flowacademy/holidaymanager/HolidayManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.holidaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayManagerApplication.class, args);
+	}
+
+}
